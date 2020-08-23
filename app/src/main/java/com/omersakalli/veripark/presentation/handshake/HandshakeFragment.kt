96da2557e1065cad56986ee331c573236ead6e05
nouvarze <osakalli97@gmail.com>
2020-08-23 17:24:30 +0300

    Added drawer menu

--- app/src/main/java/com/omersakalli/veripark/presentation/handshake/HandshakeFragment.kt
+++ app/src/main/java/com/omersakalli/veripark/presentation/handshake/HandshakeFragment.kt
@@ -1,30 +1,26 @@
 package com.omersakalli.veripark.presentation.handshake
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.liveData
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.omersakalli.veripark.R
 import com.omersakalli.veripark.databinding.HandshakeFragmentBinding
-import com.omersakalli.veripark.domain.usecase.GetHandshakeUseCase
-import dagger.android.support.DaggerFragment
 
-class HandshakeFragment() : Fragment() {
+class HandshakeFragment : Fragment() {
 
     private lateinit var binding: HandshakeFragmentBinding
     private lateinit var viewModel: HandshakeViewModel
 
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        binding = DataBindingUtil.inflate(inflater,R.layout.handshake_fragment,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.handshake_fragment, container, false)
         val view = binding.root
         return view
     }
@@ -32,7 +28,11 @@ class HandshakeFragment() : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         //viewModel = ViewModelProviders.of(requireActivity()).get(HandshakeViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.goToListButton.setOnClickListener {
+            val action =
+                HandshakeFragmentDirections.actionHandshakeFragmentToİmkbListFragment("all")
+            findNavController().navigate(action)
+        }
 
     }
 
--- app/src/main/java/com/omersakalli/veripark/presentation/imkblist/ImkbListFragment.kt
+++ app/src/main/java/com/omersakalli/veripark/presentation/imkblist/ImkbListFragment.kt
@@ -1,42 +1,30 @@
 package com.omersakalli.veripark.presentation.imkblist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.omersakalli.veripark.R
-import com.omersakalli.veripark.data.api.ImkbService
-import com.omersakalli.veripark.data.model.Handshake.HandshakeResponse
 import com.omersakalli.veripark.data.model.IMKB.ListRequest
-import com.omersakalli.veripark.data.repository.imkblist.ImkbListRepositoryImpl
-import com.omersakalli.veripark.data.repository.imkblist.datasourceimpl.ImkbListRemoteDataSourceImpl
 import com.omersakalli.veripark.databinding.FragmentImkbListBinding
-import com.omersakalli.veripark.databinding.HandshakeFragmentBinding
-import com.omersakalli.veripark.domain.repository.ImkbListRepository
-import com.omersakalli.veripark.domain.usecase.GetImkbListUseCase
-import com.omersakalli.veripark.presentation.di.Injector
 import com.omersakalli.veripark.presentation.di.core.NetModule
 import com.omersakalli.veripark.presentation.handshake.HandshakeViewModel
 import com.omersakalli.veripark.util.AES_Functions
-import retrofit2.Retrofit
-import javax.inject.Inject
 
 class ImkbListFragment : Fragment() {
 
-    //lateinit var factory: ImkbListViewModelFactory
     private lateinit var binding: FragmentImkbListBinding
     private lateinit var adapter: ListAdapter
     private lateinit var imkbListViewModel: ImkbListViewModel
     private val handshakeViewModel: HandshakeViewModel by activityViewModels()
+    private lateinit var stockPeriod: String
 
 
     override fun onCreateView(
@@ -46,29 +34,12 @@ class ImkbListFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_imkb_list, container, false)
 
-//        (requireActivity().application as Injector).createImkbListSubComponent().inject(this)
         imkbListViewModel = ViewModelProvider(this).get(ImkbListViewModel::class.java)
         val imkbService = NetModule().provideImkbService()
-        handshakeViewModel.getHandshake().observe(viewLifecycleOwner, Observer {
-            if (it != null) {
-                imkbListViewModel.getImkbListUseCase =
-                    GetImkbListUseCase(
-                        ImkbListRepositoryImpl(
-                            ImkbListRemoteDataSourceImpl(
-                                imkbService,
-                                it,
-                                ListRequest("all")
-                            )
-                        )
-                    )
-                initRecyclerView()
-            }
-        })
 
+        stockPeriod = requireArguments().getString("period", "all")
 
-
-
-
+        initRecyclerView()
         return binding.root
     }
 
@@ -81,14 +52,27 @@ class ImkbListFragment : Fragment() {
         binding.listRecyclerview.layoutManager = LinearLayoutManager(context)
         adapter = ListAdapter()
         binding.listRecyclerview.adapter = adapter
-        displayStocks()
+        displayStocks(stockPeriod)
     }
 
-    private fun displayStocks() {
+    /**
+     * Gets imkb stocks from api using handshake data and feeds it to list adapter
+     * @param stockPeriod - Listing type of stocks can be one of these (as listed in api documentation in Turkish)
+     * 	|---> all: IMKB hisse ve endeksleri almak için kullanılır.
+     * 	|---> increasing: Yükselenleri almak için kullanılır.
+     * 	|---> decreasing: Düşenleri almak için kullanılır.
+     * 	|---> volume30: IMKB hacme göre 30 listesini almak için kullanılır.
+     * 	|---> volume50: IMKB hacme göre 50 listesini almak için kullanılır.
+     * 	\---> volume100: IMKB hacme göre 100 listesini almak için kullanılır.
+     */
+    private fun displayStocks(stockPeriod: String) {
         binding.listProgressBar.visibility = View.VISIBLE
-        handshakeViewModel.getHandshake().observe(viewLifecycleOwner, Observer {handshake->
-            imkbListViewModel.getImkbList(handshake.authorization,ListRequest(AES_Functions.encrypt("all",handshake.aesKey,handshake.aesIV))).observe(viewLifecycleOwner,
-                Observer { imkb->
+        handshakeViewModel.getHandshake().observe(viewLifecycleOwner, Observer { handshake ->
+            imkbListViewModel.getImkbList(
+                handshake.authorization,
+                ListRequest(AES_Functions.encrypt(stockPeriod, handshake.aesKey, handshake.aesIV))
+            ).observe(viewLifecycleOwner,
+                Observer { imkb ->
                     if (imkb.body() != null && !imkb.body()!!.stocks.isEmpty()) {
                         adapter.setList(
                             imkb.body()!!.stocks,
@@ -102,21 +86,6 @@ class ImkbListFragment : Fragment() {
                         Toast.makeText(context, "No data available", Toast.LENGTH_LONG).show()
                     }
                 })
-        })/*
-        val responseLiveData = imkbListViewModel.getImkbList()
-        responseLiveData.observe(viewLifecycleOwner, Observer {
-            if (it != null && !it.stocks.isEmpty()) {
-                adapter.setList(
-                    it.stocks,
-                    handshakeViewModel.getHandshake().value!!.aesKey,
-                    handshakeViewModel.getHandshake().value!!.aesKey
-                )
-                adapter.notifyDataSetChanged()
-                binding.listProgressBar.visibility = View.GONE
-            } else {
-                binding.listProgressBar.visibility = View.GONE
-                Toast.makeText(context, "No data available", Toast.LENGTH_LONG).show()
-            }
-        })*/
+        })
     }
 }
