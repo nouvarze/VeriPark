637174fddbabe5ecc642fdf89d5546f2ce460f9b
nouvarze <osakalli97@gmail.com>
2020-08-23 17:24:30 +0300

    Added drawer menu

--- app/src/main/java/com/omersakalli/veripark/MainActivity.kt
+++ app/src/main/java/com/omersakalli/veripark/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.omersakalli.veripark
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.navigation.NavigationView
 import com.omersakalli.veripark.databinding.MainActivityBinding
 import com.omersakalli.veripark.presentation.di.Injector
 import com.omersakalli.veripark.presentation.handshake.HandshakeViewModel
 import com.omersakalli.veripark.presentation.handshake.HandshakeViewModelFactory
+import com.omersakalli.veripark.presentation.imkblist.ImkbListFragment
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var factory: HandshakeViewModelFactory
@@ -21,18 +25,77 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
         (application as Injector).createHandshakeSubComponent().inject(this)
 
-        viewModel=ViewModelProvider(this,factory).get(HandshakeViewModel::class.java)
+        viewModel = ViewModelProvider(this, factory).get(HandshakeViewModel::class.java)
 
-        val a=viewModel.getHandshake()
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.nav_app_bar_open_drawer_description,
+            R.string.nav_app_bar_close_drawer_description
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        a.observe(this, Observer {
-            Log.i("MYTAG","\n\nAES KEY: ${it.aesKey}\nAES IV: ${it.aesIV}\nAuth: ${it.authorization}\nlifetime ${it.lifeTime}")
-        })
+        binding.navView.setNavigationItemSelectedListener(this)
 
     }
 
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else super.onBackPressed()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        val fragment = ImkbListFragment()
+
+        when (item.itemId) {
+
+            R.id.nav_allstocks -> {
+                fragment.arguments = bundleOf("period" to "all")
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+
+            R.id.nav_rising -> {
+                fragment.arguments = bundleOf("period" to "increasing")
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+
+            R.id.nav_falling -> {
+                fragment.arguments = bundleOf("period" to "decreasing")
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+
+            R.id.nav_100 -> {
+                fragment.arguments = bundleOf("period" to "volume30")
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+
+            R.id.nav_50 -> {
+                fragment.arguments = bundleOf("period" to "volume50")
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+
+            R.id.nav_30 -> {
+                fragment.arguments = bundleOf("period" to "volume100")
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+        }
+
+
+        return true
+    }
 
 }
--- app/src/main/java/com/omersakalli/veripark/presentation/handshake/HandshakeViewModel.kt
+++ app/src/main/java/com/omersakalli/veripark/presentation/handshake/HandshakeViewModel.kt
@@ -1,8 +1,6 @@
 package com.omersakalli.veripark.presentation.handshake
 
-import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.omersakalli.veripark.domain.usecase.GetHandshakeUseCase
@@ -15,7 +13,6 @@ class HandshakeViewModel(private val handshakeUseCase: GetHandshakeUseCase) : Vi
 
         emit(handshake)
     }
-
 
 
 }
